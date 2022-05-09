340a6843d91b39880d85c17f40184eba6411c2a5
Paulo_Medeiros <paulo_alexandre_slipknot@hotmail.com>
2022-05-09 15:27:43 -0400

    atualizando a branch

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,34 @@
-test('', () => {});
+import React from 'react';
+import { createMemoryHistory } from 'history';
+import { Router } from 'react-router-dom';
+import { render, screen } from '@testing-library/react';
+import App from '../App';
+import userEvent from '@testing-library/user-event';
+
+function renderWithRouter(
+    ui,
+    { route = '/', history = createMemoryHistory({ initialEntries: [route] }) } = {},
+  ) {
+    return {
+      ...render(<Router history={ history }>{ui}</Router>),
+      history,
+    };
+  }
+
+  describe('Requisito 1 - Testando o componente App.js', () => {
+test('Testa se o topo da aplicação contém um conjunto fixo de links de navegação', () => {
+    renderWithRouter(<App />);
+
+    const firstLink = screen.getByText(/Home/i);
+    expect(firstLink).toBeInTheDocument();
+
+    const secondLink = screen.getByText(/About/i);
+    expect(secondLink).toBeInTheDocument();
+
+    const thirdLink = screen.getByText(/Favorite Pokémons/i);
+    expect(thirdLink).toBeInTheDocument();
+});
+test('Testa se a aplicação é redirecionada para a página inicial, na URL / ao clicar no link Home da barra de navegação.', () => {
+
+})
+  });
