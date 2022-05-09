e59ed4571c9148c3614f151bc622b83fa68cfcb4
Paulo_Medeiros <paulo_alexandre_slipknot@hotmail.com>
2022-05-09 17:47:09 -0400

    Adiciona os testes restantes

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -26,4 +26,30 @@ describe('Requisito 1 - Testando o componente App.js', () => {
     const { pathname } = history.location;
     expect(pathname).toBe('/');
   });
+
+  test('Testa se a aplicação é redirecionada para a página de About', () => {
+    const { history } = renderWithRouter(<App />);
+
+    const about = screen.getByText(/About/i);
+
+    fireEvent.click(about);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/about');
+  });
+
+  test('Testa se é redirecionada para a página de Pokémons Favoritados', () => {
+    const { history } = renderWithRouter(<App />);
+
+    const favoritePokemons = screen.getByText(/Favorite Pokémons/i);
+
+    fireEvent.click(favoritePokemons);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/favorites');
+  });
+
+  test('Testa se é redirecionada para a página NotFound', () => {
+    const { history } = renderWithRouter(<App />);
+
+    history.push('/xablau');
+  });
 });
