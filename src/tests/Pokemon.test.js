d77d42f1744a3e1085f3d65b94b567a0df7f58f3
Paulo_Medeiros <paulo_alexandre_slipknot@hotmail.com>
2022-05-11 09:20:02 -0400

    Adiciona testes restantes para o requisito 6

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { screen } from '@testing-library/react';
+import { screen, userEvent } from '@testing-library/react';
 import renderWithRouter from '../renderWithRouter';
 import App from '../App';
 
@@ -13,10 +13,10 @@ describe('Requisito 6 - Testa o componente Pokemon.js', () => {
     const typePokemon = screen.getByTestId('pokemon-type');
     expect(typePokemon).not.toHaveTextContent('');
 
-    const weigthPokemon = screen.getByTestId('pokemon-weight');
-    expect(weigthPokemon).toHaveTextContent(/weigth/);
-    expect(weigthPokemon).toHaveTextContent(/[0,9]/);
-    expect(weigthPokemon).toHaveTextContent(/kg/);
+    const weightPokemon = screen.getByTestId('pokemon-weight');
+    expect(weightPokemon).toHaveTextContent(/weight/);
+    expect(weightPokemon).toHaveTextContent(/[0-9]/);
+    expect(weightPokemon).toHaveTextContent(/kg/);
 
     const imgPokemon = screen.getByAltText(/sprite/i);
     expect(imgPokemon.src).not.toBe('');
@@ -36,14 +36,47 @@ describe('Requisito 6 - Testa o componente Pokemon.js', () => {
   // Teste se ao clicar no link de navegação do pokémon, é feito o redirecionamento da aplicação para a página de detalhes de pokémon.
   test('Testa ao clicar no link navegação do pokémon, redireciona a aplicação', () => {
     renderWithRouter(<App />);
+
+    const navigator = screen.getByText(/More Details/i);
+    userEvent.click(navigator);
+
+    const pokemon = screen.getByRole('heading', {
+      level: 2,
+      name: 'Pikachu Details',
+    });
+    expect(pokemon).toBeInTheDocument();
   });
 
   // Teste também se a URL exibida no navegador muda para /pokemon/<id>, onde <id> é o id do pokémon cujos detalhes se deseja ver;
   test('Testa também se a URL exibida no navegador muda para /pokemon/<id>', () => {
-    renderWithRouter(<App />);
+    const { history } = renderWithRouter(<App />);
+
+    const linkDetails = screen.getByText(/More details/i);
+    userEvent.click(linkDetails);
+
+    const { pathname } = history.location;
+    expect(pathname).toBe('/pokemons/25');
   });
 
   test('Testa se existe um ícone de estrela nos pokémons favoritados', () => {
     renderWithRouter(<App />);
+
+    // Passo 1: Simula o click no More details
+    userEvent.click(screen.getByText(/More details/));
+
+    // Passo 2: Simula o acesso ao checkbox para favoritar
+    const favoriteCheckin = screen.getByRole('checkbox');
+    expect(favoriteCheckin).toBeInTheDocument();
+
+    // Passo 3: Simula o click no Checkbox
+    userEvent.click(favoriteCheckin);
+
+    // Passo 4: Simula se o pokemon foi favoritado
+    const favoritePoke = screen.getByAltText(/is marked as favorite/);
+    expect(favoritePoke).toBeInTheDocument();
+
+    // Passo 5: Testa se tem o icon star
+    userEvent.click(favoriteCheckin);
+    expect(favoritePoke).toHaveProperty('src', 'http://localhost/star-icon.svg');
   });
 });
